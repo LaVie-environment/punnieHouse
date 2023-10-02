82741c9f3b77d4db524154d1929050d90acd2c2f
armzig <bellotaophyc@outlook.com>
2023-10-02 16:26:17 +0100

    define the markup file

--- resource-storage.tf
+++ resource-storage.tf
@@ -16,5 +16,16 @@ resource "aws_s3_bucket_website_configuration" "punnie_bucing" {
   error_document {
     key = "error.html"
   }
-  
+
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.punnie_bucket.id
+  key    = "index.html"
+  source = "${path.module}/index.html"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("${path.module}/index.html")
 }
